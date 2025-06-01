158b92a291cc69d4549abcdd3301769f6bf80c08
Parven <parven5@proton.me>
2025-06-01 12:55:29 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -135,12 +135,11 @@ in
   # System-Wide Packages
   ##############################################
   environment.systemPackages = with pkgs; [
-    wget git github-desktop bitwarden-desktop brave
-    discord spotify bleachbit vlc gimp audacity obs-studio
-    cheese lutris prismlauncher gnome-tweaks gnome-extension-manager
-    podman distrobox telegram-desktop gnome-menus gobject-introspection
-    neofetch corefonts powertop kitty xdg-utils dconf-editor
-    helix cava
+    wget git github-desktop vivaldi discord spotify bleachbit vlc 
+    gimp audacity obs-studio cheese lutris prismlauncher gnome-tweaks 
+    gnome-extension-manager podman distrobox telegram-desktop 
+    gnome-menus gobject-introspection neofetch corefonts powertop 
+    kitty xdg-utils dconf-editor helix cava
   ];
 
   nixpkgs.config.permittedInsecurePackages = [
