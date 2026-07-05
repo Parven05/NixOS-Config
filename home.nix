51591314c8cf3703924f16a396404eeddb2c799f
Parven05 <parven5@proton.me>
2026-07-05 17:14:52 +0800

    fix gitui bad credentials

--- home.nix
+++ home.nix
@@ -44,6 +44,25 @@ in
 
   services.ssh-agent.enable = true;
 
+  systemd.user.services.ssh-add-key = {
+    Unit = {
+      Description = "Add SSH key to agent on login";
+      After = [
+        "ssh-agent.service"
+        "sops-nix.service"
+      ];
+      Requires = [
+        "ssh-agent.service"
+        "sops-nix.service"
+      ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.openssh}/bin/ssh-add /home/parven/.ssh/id_ed25519";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
   # ------------------------------------------------------------------
   # SOPS
   # ------------------------------------------------------------------
