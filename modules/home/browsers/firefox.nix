be9fb49d53efc77fce4b703b178c54f97a236245
Parven <parven5@proton.me>
2026-07-18 13:17:09 +0800

    added search bar to site/index.html

--- modules/home/browsers/firefox.nix
+++ modules/home/browsers/firefox.nix
@@ -59,6 +59,11 @@ in
           installation_mode = "force_installed";
         };
 
+        "newtaboverride@agenedia.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/new-tab-override/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
         "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
           installation_mode = "force_installed";
